ed454ca843ea2ac24ed4a16887812bd39e877a37
Guy Arbitman <guy20495@gmail.com>
2025-12-22 09:10:40 +0200

    updated requirements and code

--- httpbin/core.py
+++ httpbin/core.py
@@ -29,10 +29,9 @@ from flask import (
     g,
 )
 from six.moves import range as xrange
-from werkzeug.datastructures import WWWAuthenticate, MultiDict
+from werkzeug.datastructures import WWWAuthenticate, MultiDict, Authorization
 from werkzeug.http import http_date
 from werkzeug.wrappers import Response
-from werkzeug.http import parse_authorization_header
 from flasgger import Swagger, NO_SANITIZER
 
 from . import filters
@@ -1213,7 +1212,7 @@ def digest_auth(
     authorization = request.headers.get("Authorization")
     credentials = None
     if authorization:
-        credentials = parse_authorization_header(authorization)
+        credentials = Authorization.from_header(authorization)
 
     if (
         not authorization
